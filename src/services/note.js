57365529a5c2fc0257ec12059ff9a7ab9c1035b1
Marek Zelinka <mzelinka17@gmail.com>
2023-02-17 19:47:16 +0100

    use axios

--- src/services/note.js
+++ src/services/note.js
@@ -1,39 +1,15 @@
+import axios from "axios";
+
 const baseUrl = "/api/notes";
-const headers = new Headers();
-headers.set("Content-Type", "application/json");
-
-function toJSON(response) {
-  return response.then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    return response.json();
-  });
-}
 
 export function getNotes() {
-  const response = fetch(baseUrl);
-
-  return toJSON(response);
+  return axios.get(baseUrl).then((response) => response.data);
 }
 
 export function updateNote(id, data) {
-  const response = fetch(`${baseUrl}/${id}`, {
-    method: "put",
-    headers,
-    body: JSON.stringify(data),
-  });
-
-  return toJSON(response);
+  return axios.put(`${baseUrl}/${id}`, data).then((response) => response.data);
 }
 
 export function createNote(data) {
-  const response = fetch(baseUrl, {
-    method: "post",
-    headers,
-    body: JSON.stringify(data),
-  });
-
-  return toJSON(response);
+  return axios.post(baseUrl, data).then((response) => response.data);
 }
