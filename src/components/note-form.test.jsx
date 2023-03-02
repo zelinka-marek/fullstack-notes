cb14f95ab1640dd214e99c877dbc7943d7d8492b
Marek Zelinka <mzelinka17@gmail.com>
2023-03-02 16:43:40 +0100

    test important note creation

--- src/components/note-form.test.jsx
+++ src/components/note-form.test.jsx
@@ -1,9 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { NoteForm } from "./note-form";
 
 describe("<NoteForm />", () => {
+  beforeEach(() => {
+    cleanup();
+  });
+
   it("should call onSubmit when submit button is clicked", async () => {
     const onSubmit = vi.fn();
 
@@ -19,5 +23,25 @@ describe("<NoteForm />", () => {
 
     expect(onSubmit).toHaveBeenCalledTimes(1);
     expect(onSubmit).toHaveBeenCalledWith({ content, important: false });
+  });
+
+  it("should create an important note", async () => {
+    const onSubmit = vi.fn();
+
+    render(<NoteForm onSubmit={onSubmit} />);
+
+    const content = "testing a form...";
+
+    const input = screen.getByRole("textbox", { name: /content/i });
+    await userEvent.type(input, content);
+
+    const important = screen.getByRole("checkbox", { name: /important/i });
+    await userEvent.click(important);
+
+    const submitButton = screen.getByRole("button", { name: /save/i });
+    await userEvent.click(submitButton);
+
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+    expect(onSubmit).toHaveBeenCalledWith({ content, important: true });
   });
 });
