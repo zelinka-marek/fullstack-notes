5fc8541dddd4794113bf46273430de8d9fb1dcf6
Marek Zelinka <mzelinka17@gmail.com>
2023-03-02 14:39:02 +0100

    test coverage using vitest

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./setup-tests.js",
     css: false,
+    coverage: {
+      provider: "istanbul",
+    },
   },
   server: {
     proxy: {
